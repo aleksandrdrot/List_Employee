eabd459ffac33fd25416451afe67ec489c541650
aleksandrdrot <aleksandr_drot@rambler.ru>
2023-04-04 19:42:22 +0300

    List_Employee

---
+++ src/main/java/com/drot/listemployee/ListEmployee/ListEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.drot.listemployee.ListEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListEmployeeApplication.class, args);
+	}
+
+}
